92aa89edfcf473274c5fc4ca9e59591f5cf22c9f
Ajinkya Aher <76814759+AjinkyaAher5@users.noreply.github.com>
2022-04-16 23:56:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {Route,Router, Routes} from "react-router-dom"
+import { Country } from './componets/Country';
+import {City} from './componets/City'
+import {User} from './componets/User'
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+    <Link to="/add-country">Add info</Link>
+      <Routes>
+        
+        <Route path='add-country' element={<Country/>}></Route>
+        <Route path='add-city' element={<City/>}></Route>
+        <Route path='users'element={<User/>}></Route>
+
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
